5c9f19d3b17e104ab793af768560f0b879af9b6c
sebapersson <sebastian.persson1@gmail.com>
2024-03-16 09:49:53 +0100

    Add multisites benchmarks

---
+++ SBMLImporter/run_multisites_sbmlimporter_ssa_sortingdirect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+JULIA_THREADS_TO_USE=1
+run_julia="julia"
+
+for i in {1..6}
+do
+    if [[ "$i" == '2' ]]; then
+        continue
+    fi
+    echo "Starts benchmark runs on the multisite model nr ${i}"
+    model_run="multisite${i}"
+    time ${run_julia} --threads $JULIA_THREADS_TO_USE sbmlimporter_make_benchmark.jl ${model_run} SortingDirect 1 3 3
+done
